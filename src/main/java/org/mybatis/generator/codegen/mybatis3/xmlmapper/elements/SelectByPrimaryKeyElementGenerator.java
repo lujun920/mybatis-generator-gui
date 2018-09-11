afd0375e92f79b17ae5453efdbae80ae7be861cb
baizhang <baizhang@dian.so>
2018-09-11 20:29:58 +0800

    完成Mapper id getRecord改造

--- src/main/java/org/mybatis/generator/codegen/mybatis3/xmlmapper/elements/SelectByPrimaryKeyElementGenerator.java
+++ src/main/java/org/mybatis/generator/codegen/mybatis3/xmlmapper/elements/SelectByPrimaryKeyElementGenerator.java
@@ -15,11 +15,9 @@
  */
 package org.mybatis.generator.codegen.mybatis3.xmlmapper.elements;
 
-import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.dom.xml.Attribute;
 import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
-import org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities;
 
 import static org.mybatis.generator.internal.util.StringUtility.stringHasValue;
 
@@ -39,8 +37,12 @@ public class SelectByPrimaryKeyElementGenerator extends
     public void addElements(XmlElement parentElement) {
         XmlElement answer = new XmlElement("select"); //$NON-NLS-1$
 
+        //answer.addAttribute(new Attribute(
+        //        "id", introspectedTable.getSelectByPrimaryKeyStatementId())); //$NON-NLS-1$
+
         answer.addAttribute(new Attribute(
-                "id", introspectedTable.getSelectByPrimaryKeyStatementId())); //$NON-NLS-1$
+                "id", "getRecord")); //$NON-NLS-1$
+
         if (introspectedTable.getRules().generateResultMapWithBLOBs()) {
             answer.addAttribute(new Attribute("resultMap", //$NON-NLS-1$
                     introspectedTable.getResultMapWithBLOBsId()));
@@ -64,7 +66,7 @@ public class SelectByPrimaryKeyElementGenerator extends
         }
 
         answer.addAttribute(new Attribute("parameterType", //$NON-NLS-1$
-                parameterType));
+                introspectedTable.getBaseRecordType()));
 
         context.getCommentGenerator().addComment(answer);
 
@@ -90,24 +92,30 @@ public class SelectByPrimaryKeyElementGenerator extends
                 .getAliasedFullyQualifiedTableNameAtRuntime());
         answer.addElement(new TextElement(sb.toString()));
 
-        boolean and = false;
-        for (IntrospectedColumn introspectedColumn : introspectedTable
-                .getPrimaryKeyColumns()) {
-            sb.setLength(0);
-            if (and) {
-                sb.append("  and "); //$NON-NLS-1$
-            } else {
-                sb.append("where "); //$NON-NLS-1$
-                and = true;
-            }
+        /**
+         * getRecord where 为 _parameter
+         */
+        answer.addElement(getExampleIncludeElement());
 
-            sb.append(MyBatis3FormattingUtilities
-                    .getAliasedEscapedColumnName(introspectedColumn));
-            sb.append(" = "); //$NON-NLS-1$
-            sb.append(MyBatis3FormattingUtilities
-                    .getParameterClause(introspectedColumn));
-            answer.addElement(new TextElement(sb.toString()));
-        }
+
+        //boolean and = false;
+        //for (IntrospectedColumn introspectedColumn : introspectedTable
+        //        .getPrimaryKeyColumns()) {
+        //    sb.setLength(0);
+        //    if (and) {
+        //        sb.append("  and "); //$NON-NLS-1$
+        //    } else {
+        //        sb.append("where "); //$NON-NLS-1$
+        //        and = true;
+        //    }
+        //
+        //    sb.append(MyBatis3FormattingUtilities
+        //            .getAliasedEscapedColumnName(introspectedColumn));
+        //    sb.append(" = "); //$NON-NLS-1$
+        //    sb.append(MyBatis3FormattingUtilities
+        //            .getParameterClause(introspectedColumn));
+        //    answer.addElement(new TextElement(sb.toString()));
+        //}
 
         if (context.getPlugins()
                 .sqlMapSelectByPrimaryKeyElementGenerated(answer,
